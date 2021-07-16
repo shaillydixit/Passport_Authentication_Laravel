9d9bd64eb4a5335fa6689f20e1a217bdc92f2eee
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-07-17 03:02:42 +0530

    passport authentication installed and model and migration done

---
+++ app/Models/Author.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
+
+class Author extends Model
+{
+    use HasFactory;
+    use HasApiTokens;
+
+    public $timestamps = false;
+}
